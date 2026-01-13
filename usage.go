b26f4457f1375b41df19441d89d0396ba17737a6
Quint Daenen <quint@daenen.email>
2026-01-13 11:20:23 +0100

    chore: add license header

--- usage.go
+++ usage.go
@@ -1,3 +1,21 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+// Copyright (c) 2026 Quint Daenen.
+// This file is part of go-license-header.
+//
+// go-license-header is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// go-license-header is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser Public License along with
+// go-license-header. If not, see <https://www.gnu.org/licenses/>.
+
 package main
 
 var usage = `%s checks and generates license headers in Go files.
